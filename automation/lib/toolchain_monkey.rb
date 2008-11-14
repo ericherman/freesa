9bbf3e9dce2d2ffae59843d2b7c8de9a70834e1e
Brett Neumeier <brett@neumeier.us>
2008-11-14 07:53:10 -0600

    move literate build system files to literatebuild

--- automation/lib/toolchain_monkey.rb
+++
@@ -1,5 +0,0 @@
-class ToolchainMonkey
-  def initialize(executioner)
-    @exec = executioner
-  end
-end
--- automation/spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-lib_path = File.expand_path(File.join("#{File.dirname(__FILE__)}",'..','lib'))
-$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
-
-require 'rubygems'
-gem 'rspec'
-require 'spec'        # explicitly required for autotest
-
-gem 'ruby-debug'
-require 'ruby-debug'
